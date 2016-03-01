0a22dbc0c6b1b69133e7dda7f60fffcd95c018b1
Christoher Korpela <christopher.korpela@gmail.com>
2016-02-29 22:03:43 -0500

    updates to exaxx_teleop node

--- emaxx_driver/scripts/move_emaxx_teleop.py
+++ emaxx_driver/scripts/move_emaxx_teleop.py
@@ -4,22 +4,20 @@ import rospy
 from std_msgs.msg import UInt16
 from geometry_msgs.msg import Twist
 
-steering = 90
 velocity = Twist()
 
 def move_emaxx():
     steering_pub = rospy.Publisher('/servo', UInt16, queue_size=10)
     rospy.init_node('move_emaxx_teleop', anonymous=True)
-    rospy.Subscriber('/turtle1/command_velocity', Twist, callback)
-
+    rospy.Subscriber('/turtle1/cmd_vel', Twist, callback)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-
-        steering = 90 + (velocity.angular.z * 10) 
+        steering = 90 + (velocity.angular.z * 5)
         steering_pub.publish(steering)
         rate.sleep()
 
 def callback(data):
+    global velocity
     velocity = data
 
 if __name__ == '__main__':
